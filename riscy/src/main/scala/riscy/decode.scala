c16f6e7630f7593a47354dbc7fcf6013680a8d67
mark <mmansi@utexas.edu>
2016-10-16 16:14:57 -0500

    Fixed bugs in decode module

--- riscy/src/main/scala/riscy/decode.scala
+++ riscy/src/main/scala/riscy/decode.scala
@@ -28,25 +28,33 @@ class RiscyDecode extends Module {
   io.funct3 := io.ins(14, 12)
   io.funct7 := io.ins(31, 25)
 
-  io.immI   := io.ins(31, 20)
-  io.immS   := Cat(io.ins(31, 25), io.ins(11, 7))
-  io.immB   := Cat(io.ins(31), io.ins(7), io.ins(30, 25), io.ins(11, 8))
-  io.immU   := Cat(io.ins(31), io.ins(30, 20), io.ins(19, 12))
-  io.immJ   := Cat(io.ins(31), io.ins(19, 12), io.ins(20), io.ins(30, 25), io.ins(24, 21))
+  io.immI   := Cat(Fill(20, io.ins(31)), io.ins(31, 20))
+  io.immS   := Cat(Fill(20, io.ins(31)), io.ins(31, 25), io.ins(11, 7))
+  io.immB   := Cat(Fill(19, io.ins(31)), io.ins(31), io.ins(7), io.ins(30, 25), io.ins(11, 8), UInt(0, 1))
+  io.immU   := Cat(io.ins(31, 12), UInt(0, 12))
+  io.immJ   := Cat(Fill(11, io.ins(31)), io.ins(31), io.ins(19, 12), io.ins(20), io.ins(30, 25), io.ins(24, 21), UInt(0, 1))
 }
 
 class RiscyDecodeTests(c: RiscyDecode) extends Tester(c) {
-  val input = 0xAAAAAAAA
-  poke(c.io.ins, input)
-  
+  poke(c.io.ins, 0xAAAAAAAA)
+
   step(1)
 
-  // TODO
-  //expect(c.io.op, 0x2A)  
+  expect(c.io.op, 0x2A) // DONE
+  expect(c.io.rs1, 0x15) // DONE
+  expect(c.io.rs2, 0x0A) // DONE
+  expect(c.io.rd, 0x15) // DONE
+  expect(c.io.funct3, 0x2) // DONE
+  expect(c.io.funct7, 0x55) // DONE
+  expect(c.io.immI, 0xFFFFFAAA) // DONE
+  expect(c.io.immS, 0xFFFFFAB5) // DONE
+  expect(c.io.immB, 0xFFFFFAB4) // DONE
+  expect(c.io.immU, 0xAAAAA000) // DONE
+  expect(c.io.immJ, 0xFFFAA2AA) // DONE
 }
 
 class DecodeGenerator extends TestGenerator {
   def genMod(): Module = Module(new RiscyDecode())
-  def genTest[T <: Module](c: T): Tester[T] = 
+  def genTest[T <: Module](c: T): Tester[T] =
     (new RiscyDecodeTests(c.asInstanceOf[RiscyDecode])).asInstanceOf[Tester[T]]
 }
