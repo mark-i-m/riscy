e08e88670ed3cc275fb36fc9ad3a21c7519ef629
mark <mmansi@utexas.edu>
2016-10-16 20:52:20 -0500

    Fix error

--- riscy/src/main/scala/riscy/alloc.scala
+++ riscy/src/main/scala/riscy/alloc.scala
@@ -3,18 +3,18 @@ package riscy
 import Chisel._
 
 class AllocRemap extends Bundle {
-  val reg = UInt(5)
-  val idxROB = UInt(6)
+  val reg = UInt(OUTPUT, 5)
+  val idxROB = UInt(OUTPUT, 6)
 }
 
 class RiscyAlloc extends Module {
   val io = new Bundle {
     val inst = Vec.fill(4) { Valid(new DecodeIns().flip) }
 
-    val freeROB = UInt(INPUT,6)
-    val firstROB = UInt(INPUT,6)
+    val freeROB = UInt(INPUT, 6)
+    val firstROB = UInt(INPUT, 6)
 
-    val allocRemap = Vec.fill(4) { Valid(new AllocRemap().asOutput) }
+    val allocRemap = Vec.fill(4) { Valid(new AllocRemap()) }
   }
 
 
