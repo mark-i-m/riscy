e81b05bdf7f0fa633e8d2105027694d1e178ac43
preyas3359 <preyas.shah@wisc.edu>
2016-11-05 21:13:22 -0500

    adding circular buffer

--- riscy/src/main/scala/riscy/rob.scala
+++ riscy/src/main/scala/riscy/rob.scala
@@ -72,7 +72,33 @@ class ROB extends Module {
   val remap = Module(new RegFile(32, 8, 4, i => Valid(UInt(width = 6))))
 
   // The ROB storage structure
-  val rob = Vec.fill(64) { new ROBEntry() }
+  class CirculrBufferIterator[T](buffer:Array[T], start:Int) extends Iterator[T]{
+    var idx=0
+    override def hasNext = idx<buffer.size
+    override def next()={
+      val i=idx
+      idx=idx+1
+      buffer(i)
+    } 
+  }
+  
+  class CircularBuffer[T](size:Int)(implicit m:Manifest[T]) extends Seq[T]{
+    val buffer=new Array[T](size);
+    var bIdx=0;
+  
+    override def apply (idx: Int): T = buffer((bIdx+idx) % size)
+
+    override def length = size
+
+    override def iterator= new CirculrBufferIterator[T](buffer, bIdx)
+
+    def add(e:T)= {
+      buffer(bIdx)=e
+      bIdx=(bIdx +1) % size
+    }
+  }
+  
+  val rob = new CircularBuffer(64) { new ROBEntry() }
   val head = Reg(next = UInt(width = 6))
 
   // The Architectural register file
@@ -82,83 +108,83 @@ class ROB extends Module {
 
 
 
-  // Commit logic
-  // - 4-wide in order commit
-  // - Only commit if the head of the queue has a ready destination bit
-  // - Stores should actually make their memory requests at this time
-  // - Instructions with an rd should actually write the Arch reg file
-  // - Branches that were mispredicted should trigger cleanup at this point
-  //   (TODO: possibly can be done earlier?)
-  // - Move the head pointer
-  
-  // For convenience, create label wires for the four instructions at the head
-  // of ROB.
-  //
-  // front(i) = head + i
-  val front = Vec.tabulate(4) { i => rob(head + UInt(i)) }
-
-  // Compute a simple flag that tells if an instruction could commit this cycle.
-  // This should only happen if it is at the front of the queue (first 4) AND
-  // all the instructions before it are ready to commit AND the ready bit for its
-  // destination is set AND the previous instruction is not a mispredicted branch.
-  //
-  // couldCommit(i) corresponds to front(i)
-  val couldCommit = Vec.fill(4) { Bool() }
-  for(i <- 0 until 4) {
-    couldCommit(i) := 
-      (if(i > 0) { couldCommit(i-1) } else { Bool(true) }) && 
-      front(i).rdVal.valid && 
-      (if(i > 0) { !front(i-1).isMispredicted } else { Bool(true) })
-  }
-
-  // TODO: On misprediction, clear the ROB and Remap table
-  // So an entry becomes invalid if
-  // - it is popped
-  // - it is squashed by misprediction
-
-  // If the head of the ROB is a store, tell the L/SQ to actually write to
-  // memory now that the store has committed.
-  for(i <- 0 until 4) {
-    io.stCommit(i) := rob(head + UInt(i)).isSt && couldCommit(i)
-  }
-
-  // If the head of the ROB is a mispredicted branch...
-  for(i <- 0 until 4) {
-    // TODO: this doesn't exactly work. We need to choose the first
-    // mispredicted branch, not all of them...
-    when (couldCommit(i) && front(i).isMispredicted) {
-      io.mispredPC.valid := Bool(true)
-      io.mispredPC.bits := front(i).pc
-      io.mispredTarget := front(i).rdVal.bits
-    } .otherwise {
-      io.mispredPC.valid := Bool(false)
-      io.mispredPC.bits := UInt(0)
-      io.mispredTarget := UInt(0)
-    }
-  }
-
-  // Write to the register file
-  for(i <- 0 until 4) {
-    // TODO: want to only write the last value out of all ready instructions.
-    // i.e. if multiple committing instructions want to write the same reg,
-    // only the last write should win...
-    rf.io.wPorts(i).valid := couldCommit(i) && front(i).hasRd
-    rf.io.wPorts(i).bits := front(i).rdVal.bits
-  }
-
-  // Compute the new head
-  when(couldCommit(3)) {
-    head := head + UInt(3)
-  } .elsewhen(!couldCommit(3) && couldCommit(2)) {
-    head := head + UInt(2)
-  } .elsewhen(!couldCommit(2) && couldCommit(1)) {
-    head := head + UInt(1)
-  } .elsewhen(!couldCommit(1) && couldCommit(0)) {
-    head := head + UInt(0)
-  } .otherwise {
-    head := head
-  }
-
+//  // Commit logic
+//  // - 4-wide in order commit
+//  // - Only commit if the head of the queue has a ready destination bit
+//  // - Stores should actually make their memory requests at this time
+//  // - Instructions with an rd should actually write the Arch reg file
+//  // - Branches that were mispredicted should trigger cleanup at this point
+//  //   (TODO: possibly can be done earlier?)
+//  // - Move the head pointer
+//  
+//  // For convenience, create label wires for the four instructions at the head
+//  // of ROB.
+//  //
+//  // front(i) = head + i
+//  val front = Vec.tabulate(4) { i => rob(head + UInt(i)) }
+//
+//  // Compute a simple flag that tells if an instruction could commit this cycle.
+//  // This should only happen if it is at the front of the queue (first 4) AND
+//  // all the instructions before it are ready to commit AND the ready bit for its
+//  // destination is set AND the previous instruction is not a mispredicted branch.
+//  //
+//  // couldCommit(i) corresponds to front(i)
+//  val couldCommit = Vec.fill(4) { Bool() }
+//  for(i <- 0 until 4) {
+//    couldCommit(i) := 
+//      (if(i > 0) { couldCommit(i-1) } else { Bool(true) }) && 
+//      front(i).rdVal.valid && 
+//      (if(i > 0) { !front(i-1).isMispredicted } else { Bool(true) })
+//  }
+//
+//  // TODO: On misprediction, clear the ROB and Remap table
+//  // So an entry becomes invalid if
+//  // - it is popped
+//  // - it is squashed by misprediction
+//
+//  // If the head of the ROB is a store, tell the L/SQ to actually write to
+//  // memory now that the store has committed.
+//  for(i <- 0 until 4) {
+//    io.stCommit(i) := rob(head + UInt(i)).isSt && couldCommit(i)
+//  }
+//
+//  // If the head of the ROB is a mispredicted branch...
+//  for(i <- 0 until 4) {
+//    // TODO: this doesn't exactly work. We need to choose the first
+//    // mispredicted branch, not all of them...
+//    when (couldCommit(i) && front(i).isMispredicted) {
+//      io.mispredPC.valid := Bool(true)
+//      io.mispredPC.bits := front(i).pc
+//      io.mispredTarget := front(i).rdVal.bits
+//    } .otherwise {
+//      io.mispredPC.valid := Bool(false)
+//      io.mispredPC.bits := UInt(0)
+//      io.mispredTarget := UInt(0)
+//    }
+//  }
+//
+//  // Write to the register file
+//  for(i <- 0 until 4) {
+//    // TODO: want to only write the last value out of all ready instructions.
+//    // i.e. if multiple committing instructions want to write the same reg,
+//    // only the last write should win...
+//    rf.io.wPorts(i).valid := couldCommit(i) && front(i).hasRd
+//    rf.io.wPorts(i).bits := front(i).rdVal.bits
+//  }
+//
+//  // Compute the new head
+//  when(couldCommit(3)) {
+//    head := head + UInt(3)
+//  } .elsewhen(!couldCommit(3) && couldCommit(2)) {
+//    head := head + UInt(2)
+//  } .elsewhen(!couldCommit(2) && couldCommit(1)) {
+//    head := head + UInt(1)
+//  } .elsewhen(!couldCommit(1) && couldCommit(0)) {
+//    head := head + UInt(0)
+//  } .otherwise {
+//    head := head
+//  }
+//
   // TODO: set the popped elements as not valid
 }
 
