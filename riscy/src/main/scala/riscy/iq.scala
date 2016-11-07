0e4518174cb11bd3bd88f5d2e28de047027c6ff6
preyas3359 <preyas.shah@wisc.edu>
2016-11-07 14:10:48 -0600

    Issue Queue Commit

--- riscy/src/main/scala/riscy/iq.scala
+++ riscy/src/main/scala/riscy/iq.scala
@@ -10,21 +10,35 @@ class IssueQueue extends Module {
 	}
 
 	//val eachEntry = Module ( new ShiftRegPP(() => new  ROBEntry))
-	val iqueue = Vec.fill(16) {Module (new ShiftRegPP(() => new  ROBEntry))}
+	val iqueue = Vec.fill(16) {Reg(outType = Valid(new ROBEntry))}
 //	for (i <- 0 until 16) {
 //		val iqueue(i) = Module (new ShiftRegPP(() => new  ROBEntry))
 //	}
 
-//	iqueue(io.currentLen).io.wen := io.newEntry.valid
-//	iqueue(io.currentLen).io.data := io.newEntry.bits
+	val (len, wrap) = Counter(io.newEntry.valid, 16)
+	iqueue(len).valid := io.newEntry.valid
+	iqueue(len).bits := io.newEntry.bits
+
+	io.currentLen := len
+
 
 	// TODO: issuedEntry := first allReady instruction
 	// TODO: pop when woken up
 
-//	val allReady = Vec.tabulate(16) {
-//			// TODO: speculative wakeup
-//		i => iqueue(i).io.value.rs1Val.valid && iqueue(i).io.value.rs2Val.valid
-//	}
+	val allReady = Vec.tabulate(16) {
+			// TODO: speculative wakeup
+		i => iqueue(i).valid && iqueue(i).bits.rs1Val.valid && iqueue(i).bits.rs2Val.valid
+	}
+
+	for (i <- 0 until 16) {
+		when (allReady(i)) {
+			io.issuedEntry.valid := Bool(true)
+			io.issuedEntry.bits := iqueue(i).bits
+		} .otherwise {
+			io.issuedEntry.valid := Bool(false)
+			io.issuedEntry.bits := io.newEntry.bits
+		}
+	}
 }
 
 class IssueQueueTests(c: IssueQueue) extends Tester(c) {
