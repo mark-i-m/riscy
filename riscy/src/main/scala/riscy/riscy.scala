6868d252f737391fcf25614b8b04f019335aaf5a
mark <mmansi@utexas.edu>
2016-11-09 17:51:43 -0600

    Minor updates

--- riscy/src/main/scala/riscy/riscy.scala
+++ riscy/src/main/scala/riscy/riscy.scala
@@ -4,13 +4,17 @@ import Chisel._
 
 // The whole processor!
 class Riscy extends Module {
-  val io = new Bundle { }
+  val io = new Bundle { /* No system, just a processor! */ }
 
-  var memory = Module(new BigMemory(64, 1 << 24)); // 1 gB memory
+  var memory = Module(new BigMemory(64, 1 << 24)) // 1 gB memory
 
   // TODO: wire things up
-  var fetch = Module(new Fetch());
-  var icache = Module(new ICache());
-  val alloc = Module(new RiscyAlloc());
-  val rob = Module(new ROB());
+  //val bp = Module(new BP)
+  var fetch = Module(new Fetch)
+  var icache = Module(new ICache)
+  val alloc = Module(new RiscyAlloc)
+  val rob = Module(new ROB)
+  //val issue = Module(new IssueStage)
+  //val lsq = Module(new LSQ)
+  //val fus... execution?
 }
