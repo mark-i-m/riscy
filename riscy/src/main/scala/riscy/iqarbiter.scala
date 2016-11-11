4f60f49d209473a6c9b5d8a61d993c8e64971849
preyas3359 <preyas.shah@wisc.edu>
2016-11-10 20:06:38 -0600

    commiting final arbiter

--- riscy/src/main/scala/riscy/iqarbiter.scala
+++ riscy/src/main/scala/riscy/iqarbiter.scala
@@ -35,19 +35,85 @@ class IqArbiter extends Module {
 	      }
 	
 	//To track length of the queue once instructions have been assigned to queues
-	val iqLenIncr = Vec.fill(5) {Vec.fill(4) { UInt(INPUT, 4)}}
-	iqLenIncr(0) := io.iqLen
 	val min1 = UInt(width = 2)
 	val min2 = UInt(width = 2)
+	val max1 = UInt(width = 2)
+	val max2 = UInt(width = 2)
+	val finalMin = Vec.fill(4) {UInt(width = 2)}
+	val minDiff = Vec.fill(3) {UInt(width = 4)}
 	
+	// below logic is sorting all four iq lengths
+	// I have written nested loop but it was not working
+	// also we have to follow this method of sorting as
+	// we can not increment queue lengths on the go without losing a cycle
+	when (io.iqLen(0) <= io.iqLen(1)) {
+		min1 := UInt(0)
+		max1 := UInt(1)
+	} .otherwise {
+		min1 := UInt(1)
+		max1 := UInt(0)
+	}
+	when (io.iqLen(2) <= io.iqLen(3)) {
+		min2 := UInt(2)
+		max2 := UInt(3)
+	} .otherwise {
+		min2 := UInt(3)
+		max2 := UInt(2)
+	}
+	when ((min1 <= min2) && (max1 <= min2)) {
+		finalMin(0) := min1
+		finalMin(1) := max1
+		finalMin(2) := min2
+		finalMin(3) := max2
+	} .elsewhen ((min1 <= min2) && !(max1 <= min2) && (max1 <= max2)) {
+		finalMin(0) := min1
+		finalMin(1) := min2
+		finalMin(2) := max1
+		finalMin(3) := max2
+	} .elsewhen ((min1 <= min2) && !(max1 <= min2) && !(max1 <= max2)) {
+		finalMin(0) := min1
+		finalMin(1) := min2
+		finalMin(2) := max2
+		finalMin(3) := max1
+	} .elsewhen (!(min1 <= min2) && (max2 <= min1)) {
+		finalMin(0) := min2
+		finalMin(1) := max2
+		finalMin(2) := min1
+		finalMin(3) := max1
+	} .elsewhen (!(min1 <= min2) && !(max2 <= min1) && (max1 <= max2)) {
+		finalMin(0) := min2
+		finalMin(1) := min1
+		finalMin(2) := max1
+		finalMin(3) := max2
+	} .otherwise {
+		finalMin(0) := min2
+		finalMin(1) := min1
+		finalMin(2) := max2
+		finalMin(3) := max1
+	}
+
+        for (i <- 0 until 3) {
+		minDiff(i) := io.iqLen(finalMin(i+1)) - io.iqLen(finalMin(0))
+	}
+
 	for (i <- 0 until 4) {
 		io.allocIQ(i).inst.bits := io.inst(i).bits
 		io.allocIQ(i).inst.valid := io.inst(i).valid
-//		when (iqLenIncr(i)(0) <= iqLenIncr(i)(1)) {min1 := UInt(0)} .otherwise {min1 := UInt(1)}
-//		when (iqLenIncr(i)(2) <= iqLenIncr(i)(3)) {min2 := UInt(2)} .otherwise {min2 := UInt(3)}
-//		when (min1 <= min2) {io.allocIQ(i).iqNum := min1} .otherwise {io.allocIQ(i).iqNum := min2}	 		
-		iqLenIncr(i+1) := iqLenIncr(i)
-		iqLenIncr(i+1)(io.allocIQ(i).iqNum) := iqLenIncr(i)(io.allocIQ(i).iqNum) + UInt(1)
+		
+		// Logic to issue instructions to different iqs
+		// if there is a stall signal set, top issue module
+		// should not issue any instructions to any iqs
+		when (UInt(i) < minDiff(0)) {
+			io.allocIQ(i).iqNum := finalMin(0)
+		} .elsewhen (UInt(i) < minDiff(1)) {
+			io.allocIQ(i).iqNum := finalMin(1)
+		} .elsewhen (UInt(i) < minDiff(2)) {
+			io.allocIQ(i).iqNum := finalMin(2)
+		} .otherwise {
+			io.allocIQ(i).iqNum := finalMin(3)
+		}
+
+		// Logic to generate the entry for LS Buffer
 		when (io.inst(i).bits.op === UInt(0x00)) {
 			io.addBuf(i).valid := Bool(true)
 			io.addBuf(i).bits.robLoc := io.inst(i).bits.entry
