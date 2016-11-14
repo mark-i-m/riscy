d1b50c0acac8b0c4de3de20ba6d6b2e58ca3eaf7
mark <mmansi@utexas.edu>
2016-11-13 20:49:52 -0600

    Stall logic

--- riscy/src/main/scala/riscy/stall.scala
+++ riscy/src/main/scala/riscy/stall.scala
@@ -4,12 +4,57 @@ import Chisel._
 
 class RiscyStall extends Module {
   val io = new Bundle {
-    // TODO
+    // ROB and Arbiter should set their StallReq flag to signal that they would
+    // like previous stages to stall while they continue to run.
+
+    // Stages that need to stall
+    val robStallReq =     Bool(INPUT)
+    val arbiterStallReq = Bool(INPUT)
+
+    // Signals that a stage should stall
+    val fetchStall = Bool(OUTPUT)
+    val allocStall = Bool(OUTPUT)
+    val robStall   = Bool(OUTPUT)
   }
+
+  io.fetchStall := io.allocStall
+  io.allocStall := io.robStall || io.robStallReq
+  io.robStall   := io.arbiterStallReq
 }
 
 class RiscyStallTests(c: RiscyStall) extends Tester(c) {
-  println("TODO")
+  def pokeFlags(rob: Boolean, arb: Boolean) = {
+    poke(c.io.robStallReq, rob)
+    poke(c.io.arbiterStallReq, arb)
+
+    step(0)
+  }
+
+  def expectFlags(fetch: Boolean, alloc: Boolean, rob: Boolean) = {
+    expect(c.io.fetchStall, fetch)
+    expect(c.io.allocStall, alloc)
+    expect(c.io.robStall,   rob)
+  }
+
+  // nobody stalls
+  pokeFlags(false, false)
+  expectFlags(false, false, false)
+
+  // rob requests stall => fetch and alloc must stall
+  pokeFlags(true, false)
+  expectFlags(true, true, false)
+
+  // nobody stalls
+  pokeFlags(false, false)
+  expectFlags(false, false, false)
+
+  // arbiter requests stall => rob, fetch, and alloc must stall
+  pokeFlags(false, true)
+  expectFlags(true, true, true)
+
+  // arbiter requests stall => rob, fetch, and alloc must stall
+  pokeFlags(true, true)
+  expectFlags(true, true, true)
 }
 
 class StallGenerator extends TestGenerator {
