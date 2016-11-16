138ae724b1b2a6554d69710609a41818cb5f0d6d
mark <mmansi@utexas.edu>
2016-11-16 13:43:11 -0600

    Started integration

--- riscy/src/main/scala/riscy/riscy.scala
+++ riscy/src/main/scala/riscy/riscy.scala
@@ -8,13 +8,49 @@ class Riscy extends Module {
 
   var memory = Module(new BigMemory(64, 1 << 24)) // 1 gB memory
 
-  // TODO: wire things up
-  //val bp = Module(new BP)
+  val bp = Module(new BP)
   var fetch = Module(new Fetch)
-  var icache = Module(new ICache)
+  val decode = Array.fill(4)(Module(new RiscyDecode))
   val alloc = Module(new RiscyAlloc)
   val rob = Module(new ROB)
-  //val issue = Module(new IssueStage)
-  //val lsq = Module(new LSQ)
-  //val fus... execution?
+  val issue = Module(new IssueStage)
+  val lsq = Module(new LSQ)
+  val exec = Array.fill(4)(Module(new Execute))
+  val stall = Module(new Stall)
+
+  // TODO: hook up ICache and Memory
+  fetch <> memory
+
+  // TODO: hook up BP and Fetch
+  fetch <> bp
+
+  // branch misprediction signals from ROB to Fetch
+  fetch.io.isBranchMispred := rob.io.mispredPC.valid
+  fetch.io.branchMispredTarget := rob.io.mispredTarget
+
+  for(i <- 0 until 4) {
+    // instructions from Fetch to Decode
+    decode(i).io.ins := fetch.io.insts(i)
+
+    // decoded instructions to Allocation
+    alloc.io.inst(i) := decode(i).io.decoded
+  }
+
+  // Hook up all signals between Allocation and ROB
+  alloc <> rob
+
+  // TODO: Hook up Allocation and IssueStage
+  alloc <> issue
+
+  // TODO: Hook up IssueStage and LSQ (Addr Q)
+  issue <> lsq
+
+  // TODO: Hook up ROB WB and ROB
+  exec <> rob
+
+  // Hook up LSQ to ROB, so we can commit stores
+  lsq.io.stCommit := rob.io.stCommit
+
+  // TODO: hook up DCache and Memory
+  lsq <> memory
 }
